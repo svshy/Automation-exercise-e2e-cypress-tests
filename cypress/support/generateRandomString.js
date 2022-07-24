93c4e3c0560f8a6b1afb724e80079af0f81ee882
svshy <dominik.such.96@gmail.com>
2022-07-24 22:57:24 +0200

    Replace random string function with Faker.js

--- cypress/support/generateRandomString.js
+++
@@ -1,9 +0,0 @@
-export function generateRandomString() {
-  let randomString = "";
-  const chars = "abcdefghijklmnopqrstuvwxyz";
-  const randomStringLength = Math.floor(Math.random() * 15 + 6);
-  for (let i = 0; i < randomStringLength; i++) {
-    randomString += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return randomString;
-}
